dee7187a8f021bf60cbe7ceb988042b78256809a
Georgii Zaharov <zgr05.gz@gmail.com>
2025-01-10 00:53:14 +0300

    Добавлена навигация, простой шаблон, цветовая схема (доработать настройки)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,10 @@ android {
 }
 
 dependencies {
+    // Делает навигацию с переключением окон
+    implementation("androidx.navigation:navigation-compose:2.7.2")
 
+    // Делает красивые значки
     implementation("androidx.compose.material:material-icons-extended:1.4.0")
 
     implementation(libs.androidx.core.ktx)
@@ -51,6 +54,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/schedlue/MainActivity.kt
+++ app/src/main/java/com/example/schedlue/MainActivity.kt
@@ -5,20 +5,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
@@ -32,6 +38,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -44,57 +56,118 @@ class MainActivity : ComponentActivity() {
             val isDarkTheme = isSystemInDarkTheme()
             val colors = if (isDarkTheme) darkColorScheme() else lightColorScheme() //TODO: add settings theme
             MaterialTheme(colorScheme = colors) {
-                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-                val scope = rememberCoroutineScope()
-                ModalNavigationDrawer(
-                    drawerState=drawerState,
-                    drawerContent = {
-                        SlidingPanel(Modifier.statusBarsPadding())
-                    },
-                    content = {
-                        Scaffold(
-                            modifier = Modifier.fillMaxSize(),
-                            content = { innerPadding ->
-                                MainContent(modifier = Modifier.padding(innerPadding))
-                            },
-                            topBar = { TopBar(drawerState, scope) }
-                        )
-                    }
-                )
+                AppNavHost()
             }
         }
     }
 }
 
 @Composable
-fun MainContent(modifier: Modifier) {
-    Text(modifier = modifier, text = "Hello world")
+fun AppNavHost() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home" // Начальный экран
+    ) {
+        composable("home") { HomeScreen(navController) }
+        composable("settings") { SettingsScreen(navController) }
+    }
+}
+
+@Composable
+fun AppScaffold(
+    navController: NavController,
+    title: String,
+    content: @Composable (PaddingValues) -> Unit
+)
+{
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState=drawerState,
+        drawerContent = {
+            SlidingPanel(Modifier.statusBarsPadding())
+        },
+        content = {
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                content = content,
+                topBar = { TopBar(title, drawerState, scope, navController) }
+            )
+        }
+    )
+}
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    AppScaffold(
+        navController,
+        ""
+    ) {innerPadding ->
+        Text(text="Home Screen", modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun SettingsScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            modifier = Modifier.clickable {
+                navController.popBackStack() // идем туда откуда пришли
+            },
+            imageVector = Icons.Filled.ArrowBackIosNew,
+            contentDescription = "Back button",
+            tint = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Тут будут настройки",
+            color = MaterialTheme.colorScheme.onBackground
+
+        )
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar(state: DrawerState, scope: CoroutineScope) {
-    TopAppBar(
-        title = { Text("Smth") },
-        modifier = Modifier.fillMaxWidth(), // Используем только ширину
-        navigationIcon = {
-            Icon(
-                modifier = Modifier.clickable {
-                    scope.launch {
-                        if (state.isOpen) state.close() else state.open()
-                    }
-                },
-                imageVector = Icons.Filled.Menu,
-                contentDescription = "Menu"
-            )
-        },
-        actions = {
-            Icon(
-                imageVector = Icons.Filled.Settings,
-                contentDescription = "Settings"
-            )
-        }
-    )
+fun TopBar(title: String, state: DrawerState, scope: CoroutineScope, navController: NavController) {
+    Column {
+        TopAppBar(
+            title = { Text("Smth") },
+            modifier = Modifier.fillMaxWidth(), // Используем только ширину
+            navigationIcon = {
+                Icon(
+                    modifier = Modifier.clickable {
+                        scope.launch {
+                            if (state.isOpen) state.close() else state.open()
+                        }
+                    },
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Menu"
+                )
+            },
+            actions = {
+                Icon(
+                    modifier = Modifier.clickable {
+                        navController.navigate("settings")
+                    },
+                    imageVector = Icons.Filled.Settings,
+                    contentDescription = "Settings"
+                )
+            }
+        )
+        // Горизонтальная полоса
+        HorizontalDivider(
+            color = MaterialTheme.colorScheme.onSurface, // Полупрозрачная линия
+            thickness = 1.dp // Толщина линии
+        )
+    }
 }
 
 @Composable
